7eec6de7843ae3eec9c32ceab31a5ecae8b2bc9b
CarlosGonzalezTorres <cargon16@ucm.es>
2020-05-27 13:37:09 +0200

    Accidentes Aéreos

---
+++ Ejercicio07/Ejercicio07/Source.cpp
@@ -0,0 +1,79 @@
+﻿// Nombre del alumno Carlos González Torres
+// Usuario del Juez F26
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "stack_eda.h"
+#include <vector>
+
+struct accidente {
+    std::string Fecha;
+    int victimas;
+};
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int lineas;
+    bool encontrado = false;
+
+    std::cin >> lineas;
+    if (!std::cin)
+        return false;
+
+    std::vector<accidente> v(lineas);
+    stack<accidente>  anteriores;
+    for (int i = 0; i < lineas; ++i)
+        std::cin >> v[i].Fecha >> v[i].victimas;
+
+    for (int j = 0; j < lineas; ++j) {
+        encontrado = false;
+        if (anteriores.empty()) {
+            std::cout << "NO HAY" << std::endl;
+            anteriores.push(v[j]);
+        }
+        else {
+            while (!anteriores.empty() && !encontrado) {
+                if (anteriores.top().victimas <= v[j].victimas)
+                    anteriores.pop();
+                else encontrado = true;
+            }
+            if (encontrado) std::cout << anteriores.top().Fecha << std::endl;
+            else std::cout << "NO HAY" << std::endl;
+
+            anteriores.push(v[j]);
+        }
+    }
+    std::cout << "---" << std::endl;
+
+    // escribir sol
+
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
---
+++ Ejercicio5/Ejercicio5/Source.cpp
@@ -0,0 +1,71 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "set_eda.h"
+
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    char tipo;
+    int k;
+    std::cin >> tipo;
+    if (!std::cin)
+        return false;
+
+    std::cin >> k;
+    if (tipo == 'P') {
+        std::string p ="";
+        set<std::string> s;
+
+        std::cin >> p;
+        while (p != "FIN") {
+            s.insert(p);
+            std::cin >> p;
+        }
+    }
+    else {
+        int n;
+        set<int> s;
+
+        std::cin >> n;
+        while (n != -1) {
+            s.insert(n);
+            std::cin >> n;
+        }
+    }
+
+
+    // escribir sol
+
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}

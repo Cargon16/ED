880c680d4b88a565317a03ba4ac50128152801fe
CarlosGonzalezTorres <cargon16@ucm.es>
2020-05-28 20:05:11 +0200

    el alumno afortunado

---
+++ Ejercicio12/Ejercicio11/Source.cpp
@@ -0,0 +1,55 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "deque_eda.h"
+
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int alumn, salto;
+
+    std::cin >> alumn >> salto;
+    if (alumn == 0 && salto == 0)
+        return false;
+
+    deque<int>cola;
+    for (int i = 1; i <= alumn; ++i) {
+        cola.push_back(i);
+    }
+    cola.alumnoAfortunado(salto);
+    std::cout << cola.front() << std::endl;
+
+    // escribir sol
+
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}

d8dfd62c0d3e17a6b4dc6f36e801e328b6a954fd
CarlosGonzalezTorres <cargon16@ucm.es>
2020-06-18 18:21:47 +0200

    Ejercicios ED

---
+++ Ejercicio25/Ejercicio25/Source.cpp
@@ -0,0 +1,89 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "bintree_eda.h"
+
+bool esPrimo(int numero) {
+    // Casos especiales
+    if (numero == 0 || numero == 1 || numero == 4) return false;
+    for (int x = 2; x < numero / 2; x++) {
+        if (numero % x == 0) return false;
+    }
+    // Si no se pudo dividir por ninguno de los de arriba, sí es primo
+    return true;
+}
+
+// función que resuelve el problema
+void resolver(const bintree<int> &a, int altura, std::pair<int, int> &sol) {
+    std::pair<int, int> iz = { -1, -1 }, der = { -1, -1 };
+    if (!a.empty()) {
+        if (a.left().empty() && a.right().empty()) {
+            if (a.root() % 7 == 0 && (altura + 1 < sol.second || sol.second == -1) && !esPrimo(a.root())) {
+                sol.first = a.root();
+                sol.second = altura + 1;
+            }
+        }
+        else {
+            if (!a.left().empty() && !esPrimo(a.root()) )
+                resolver(a.left(), altura + 1, iz);
+            if (!a.right().empty() && !esPrimo(a.root()))
+                resolver(a.right(), altura + 1, der);
+
+            if (iz.first != -1) {
+                sol.first = iz.first;
+                sol.second = iz.second;
+            }
+            else {
+                sol.first = der.first;
+                sol.second = der.second;
+            }
+            if (a.root() % 7 == 0 && (altura + 1 < sol.second || sol.second == -1) && !esPrimo(a.root())) {
+                sol.first = a.root();
+                sol.second = altura + 1;
+            }
+        }
+    }
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    bintree<int> a;
+    a = leerArbol(-1);
+    std::pair<int, int> p = { -1, -1 };
+    int altura = 0;
+    resolver(a, altura, p);
+    if (p.first == -1) std::cout << "NO HAY" << std::endl;
+    else std::cout << p.first << " " << p.second << std::endl;
+
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
---
+++ Ejercicio30/Ejercicio30/Source.cpp
@@ -0,0 +1,101 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "treemap_eda.h"
+#include "list_eda.h"
+
+// función que resuelve el problema
+void resolver(map<std::string, std::pair<std::string, char>> &d, const map<std::string, std::string> &d2, list<std::string> &add, list<std::string>& del, list<std::string>& mod) {
+    for (auto it = d2.cbegin(); it != d2.cend(); ++it) {
+        //char c = it->clave;
+        if (d.find(it->clave) == d.end()) { 
+            d.insert({ it->clave, {it->valor, '+' } }); 
+        }
+        else {
+            if (d.find(it->clave)->valor.first != it->valor) d.find(it->clave)->valor.second = '*';
+            if (d.find(it->clave)->valor.first == it->valor) d.find(it->clave)->valor.second = '/';
+        }
+    }
+    for (auto it2 = d.begin(); it2 != d.end(); ++it2) {
+        if (it2->valor.second == '-') del.push_back(it2->clave);
+        if(it2->valor.second == '+') add.push_back(it2->clave);
+        if (it2->valor.second == '*') mod.push_back(it2->clave);
+    }
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    map<std::string, std::pair<std::string, char>> d;
+    map<std::string, std::string> d2;
+    list<std::string> add, mod, del;
+    
+    std::string s, c, v;
+    std::getline(std::cin, s);
+    std::stringstream s1(s);
+    while (s1 >> c) {
+        s1 >> v;
+        d.insert({ c, {v, '-' }});
+    }
+    std::getline(std::cin, s);
+    std::stringstream s2(s);
+    while (s2 >> c) {
+        s2 >> v;
+        d2.insert({ c, v});
+    }
+
+    resolver(d, d2, add, del, mod);
+    if (add.empty() && del.empty() && mod.empty()) std::cout << "Sin cambios" << std::endl;
+    else {
+        if (!add.empty()) {
+            std::cout << "+ ";
+            for (auto it = add.cbegin(); it != add.cend(); ++it) std::cout << *it << " ";
+            std::cout << std::endl;
+        }
+        if (!del.empty()) {
+            std::cout << "- ";
+            for (auto it = del.cbegin(); it != del.cend(); ++it) std::cout << *it << " ";
+            std::cout << std::endl;
+        }
+        if (!mod.empty()) {
+            std::cout << "* ";
+            for (auto it = mod.cbegin(); it != mod.cend(); ++it) std::cout << *it << " ";
+            std::cout << std::endl;
+        }
+    }
+    std::cout << "---" << std::endl;
+
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    std::cin.ignore();
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
